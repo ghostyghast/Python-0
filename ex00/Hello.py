063412baf36b3574075ea72d0d109abe1dac679e
ghostyghast <antoinethebestgamer@gmail.com>
2023-09-20 17:40:02 +0800

    working on ex01, checking if scientific number is formatted properly

--- ex00/Hello.py
+++ ex00/Hello.py
@@ -8,13 +8,9 @@ ft_list[1] = "world!"
 y = list(ft_tuple)
 y[1] = 'Malaysia!'
 ft_tuple = tuple(y)
-ft_set.discard("tutu!")
 ft_set.add("Kuala Lumpur")
+ft_set.remove("tutu!")
 ft_dict.update({"Hello" : "42KL!"})
-
-# ft_tuple = ("Hello", "toto!")
-# ft_set = {"Hello", "tutu!"}
-# ft_dict = {"Hello" : "titi!"}
 
 print(ft_list)
 print(ft_tuple)
--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,2 +1,5 @@
 import time
-import datetime
+from datetime import datetime
+
+print("Seconds since  since Janurary 1, 1970: {:,.4F} or {:e}".format(time.time(), time.time()))
+print(datetime.now().strftime("%B %d %Y"))
