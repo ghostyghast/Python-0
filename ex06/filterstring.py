59975a6870382adbed09b47c72bfb1f9495c482f
ghostyghast <antoinethebestgamer@gmail.com>
2023-09-21 22:40:46 +0800

    finished ex06

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -1,5 +1,6 @@
 from sys import argv
 from string import punctuation
+from ft_filter import ft_filter
 
 
 def correct_arg(string):
@@ -15,17 +16,11 @@ def func(x, num):
     return lambda x, num: True if len(x) > num else False
 
 
-def filterstring(string, num):
-    words = string.split()
-    new_lst = [x for x in words if func(x, num)]
-    return (new_lst)
-
-
 def main():
     assert len(argv) == 3, "invalid argument amount"
     assert argv[2].isalpha, "arguments invalid"
     assert correct_arg(argv[1]), "arguments invalid"
-    print(filterstring(argv[1], int(argv[2])))
+    print(ft_filter(lambda word, num=int(argv[2]): len(word) > num, argv[1].split()))
 
 
 if __name__ == "__main__":
