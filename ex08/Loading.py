cf41d5c1c38bb2ea6be187f9d86af09947dd1465
ghostyghast <antoinethebestgamer@gmail.com>
2023-09-22 14:06:38 +0800

    finished ex08

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,2 +1,9 @@
-def ft_tqdm() -> None:
-    pass
+def ft_tqdm(nums) -> None:
+    length = len(nums)
+    for x in nums:
+        percent = (x / length) * 100
+        bar = '█' * int(percent) + ' ' * (100 - int(percent))
+        print(f"\r {percent:.0F}%|{bar}| {x}/{length}", end='\r')
+        yield x
+    bar = '█' * 100
+    print(f"\r100%|{bar}| {length}/{length}")
--- ex08/test.py
+++ ex08/test.py
@@ -1,8 +1,8 @@
 from time import sleep
 from tqdm import tqdm
 from Loading import ft_tqdm
-# for elem in ft_tqdm():
-#     sleep(0.005)
+for elem in ft_tqdm(range(1000)):
+    sleep(0.005)
 print()
-for elem in tqdm(range(333)):
+for elem in tqdm(range(1000)):
     sleep(0.005)
